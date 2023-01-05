e8670825aecec13291ad515b4d654c356dcbc881
enes-96 <111685648+enes-96@users.noreply.github.com>
2023-01-05 22:52:03 +0000

    not done made simple locig

---
+++ script.js
@@ -0,0 +1,47 @@
+"use strict";
+const modal = document.getElementById("modal");
+const overlay = document.querySelector(".overlay");
+
+let myLibrary = [];
+
+function addBookToLibrary() {
+  const parentList = document.querySelector("ul");
+
+  for (const books in myLibrary) {
+    const newItem = document.createElement("li");
+    parentList.appendChild(newItem);
+    newItem.innerText = myLibrary[books];
+  }
+}
+
+const addBookBtn = document
+  .querySelector("button")
+  .addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
+  });
+
+function Book(author, name, pages) {
+  this.author = author;
+  this.name = name;
+  this.pages = pages;
+}
+
+const submitButton = document
+  .getElementById("submit")
+  .addEventListener("click", (event) => {
+    event.preventDefault();
+    const bookAuthor = document.getElementById("bookAuthor").value;
+    const bookTitle = document.getElementById("bookTitle").value;
+    const bookPages = document.getElementById("bookPages").value;
+
+    const newBook = new Book(bookAuthor, bookTitle, bookPages);
+
+    modal.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
+
+    myLibrary.push(newBook);
+    addBookToLibrary();
+
+    console.log(myLibrary);
+  });
