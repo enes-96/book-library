020602f7de9bed5e59f80b82a85836b9a033eb1d
Seyit Enes <seyit7enes@gmail.com>
2023-01-06 08:53:48 +0100

    almost done with logic

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 "use strict";
+
 const modal = document.getElementById("modal");
 const overlay = document.querySelector(".overlay");
 
 let myLibrary = [];
+let haveRead = false;
 
 function addBookToLibrary() {
   const parentList = document.querySelector("ul");
 
   for (const books in myLibrary) {
     const newItem = document.createElement("li");
+    newItem.style.display = "flex";
     parentList.appendChild(newItem);
-    newItem.innerText = myLibrary[books];
-  }
-}
+    newItem.innerHTML = myLibrary[books];
+    const deleteBtn = document.createElement("button");
+    newItem.appendChild(deleteBtn);
+    deleteBtn.innerHTML = "x";
+    deleteBtn.addEventListener("click", () => {
+      newItem.remove();
+    });
 
+    const readBtn = document.createElement("button");
+    newItem.appendChild(readBtn);
+    readBtn.innerHTML = "Read";
+    readBtn.addEventListener("click", () => {
+      if (haveRead == true) {
+        return (haveRead = false);
+      } else {
+        return (haveRead = true);
+      }
+    });
+  }
+  return (myLibrary = []);
+}
 const addBookBtn = document
   .querySelector("button")
   .addEventListener("click", () => {
@@ -21,11 +41,10 @@ const addBookBtn = document
     overlay.classList.toggle("hidden");
   });
 
-function Book(author, name, pages) {
+function Book(author, title, pages) {
   this.author = author;
-  this.name = name;
+  this.title = title;
   this.pages = pages;
-  //RETURN A STRING FOR THE OUTPUT LIST 
 }
 
 const submitButton = document
@@ -35,14 +54,24 @@ const submitButton = document
     const bookAuthor = document.getElementById("bookAuthor").value;
     const bookTitle = document.getElementById("bookTitle").value;
     const bookPages = document.getElementById("bookPages").value;
+    const radioButtonRead = document.getElementById("doneReading");
 
     const newBook = new Book(bookAuthor, bookTitle, bookPages);
 
     modal.classList.toggle("hidden");
     overlay.classList.toggle("hidden");
 
-    myLibrary.push(newBook);
-    addBookToLibrary();
+    if (radioButtonRead.checked) {
+      haveRead = true;
+      myLibrary.push(
+        `Author: ${newBook.author} | Title: ${newBook.title} | Pages: ${newBook.pages} | Read: ${haveRead}`
+      );
+    } else {
+      haveRead = false;
+      myLibrary.push(
+        `Author: ${newBook.author} | Title: ${newBook.title} | Pages: ${newBook.pages} | Read: ${haveRead}`
+      );
+    }
 
-    console.log(myLibrary);
+    addBookToLibrary();
   });
