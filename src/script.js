1427d2dbbd29db4d4cc4adc20b8a721db9547559
Enes <seyit7enes@gmail.com>
2023-01-07 19:05:06 +0100

    making the read button

--- src/script.js
+++ src/script.js
@@ -3,32 +3,59 @@ const table = document.querySelector("tbody");
 
 function addBookToLibrary() {
   const parentList = document.createElement("tr");
+  parentList.classList.add("text-center");
   table.appendChild(parentList);
-  parentList.classList.add("px-6", "py4");
 
   myLibrary.forEach((book) => {
     const newItem = document.createElement("td");
+    newItem.classList.add("px-6", "py-4", "text-xl");
     parentList.appendChild(newItem);
     newItem.innerHTML = book;
   });
 
-  const deleteButton = document.createElement("td");
-  const checkRead = document.createElement("td");
+  const checkIfRead = function () {
+    const toggleRead = document.createElement("button");
+    parentList.appendChild(toggleRead);
+    const hadReadButton = document.querySelector("#doneReading");
+    if (hadReadButton.checked) {
+      toggleRead.innerText = "Read";
+    }
+    if (!hadReadButton.checked) {
+      toggleRead.innerText = "Not Read";
+    }
+  };
+  checkIfRead();
 
+  const deleteWrapper = document.createElement("td");
+  parentList.appendChild(deleteWrapper);
+  deleteWrapper.classList.add("relative");
+
+  const deleteButton = document.createElement("td");
+  deleteWrapper.appendChild(deleteButton);
+  deleteButton.classList.add(
+    "bg-red-700",
+    "font-medium2",
+    "rounded-lg",
+    "px-5",
+    "py-1.5",
+    "cursor-pointer",
+    "mr-2",
+    "mb-2",
+    "text-white",
+    "absolute",
+    "top-1/2",
+    "left-1/2",
+    "transform",
+    "-translate-x-1/2",
+    "-translate-y-1/2"
+  );
+  deleteButton.innerHTML = "Delete";
   deleteButton.addEventListener("click", () => {
     parentList.remove();
   });
-
-  parentList.appendChild(deleteButton);
-  deleteButton.innerHTML = "Delete";
-  deleteButton.style.cursor = "pointer";
-  parentList.appendChild(checkRead);
-  checkRead.innerHTML = "Read";
-  checkRead.style.cursor = "pointer";
   // eslint-disable-next-line no-return-assign
-  return (myLibrary = []);
+  myLibrary = [];
 }
-
 const modal = document.getElementById("modal");
 const overlay = document.querySelector(".overlay");
 
@@ -42,15 +69,11 @@ function Book(author, title, pages) {
   this.title = title;
   this.pages = pages;
   // eslint-disable-next-line func-names, consistent-return
-  this.hadread = function () {
-    const hadReadButton = document.querySelector("#doneReading");
-    if (hadReadButton.checked) return true;
-    if (!hadReadButton.checked) return false;
-  };
 }
 
 document.getElementById("submit").addEventListener("click", (event) => {
   event.preventDefault();
+
   const bookAuthor = document.getElementById("bookAuthor").value;
   const bookTitle = document.getElementById("bookTitle").value;
   const bookPages = document.getElementById("bookPages").value;
@@ -59,9 +82,8 @@ document.getElementById("submit").addEventListener("click", (event) => {
   myLibrary.push(
     newBook.author,
     newBook.title,
-    newBook.pages,
+    newBook.pages
     // eslint-disable-next-line comma-dangle
-    newBook.hadread()
   );
   modal.classList.toggle("hidden");
   overlay.classList.toggle("hidden");
